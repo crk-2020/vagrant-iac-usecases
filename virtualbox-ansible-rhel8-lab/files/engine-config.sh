38606a895765fb634ef400995d935ec3dea85974
ginigangadharan <net.gini@gmail.com>
2021-11-29 17:33:43 +0800

    amended

--- virtualbox-ansible-rhel8-lab/files/engine-config.sh
+++ virtualbox-ansible-rhel8-lab/files/engine-config.sh
@@ -2,8 +2,3 @@ sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/
 sudo systemctl restart sshd
 
 sudo python3 -m pip install pip --upgrade
-
-# add ansible user
-sudo useradd ansible
-echo -e 'ansible\nansible' | sudo passwd ansible
-echo 'ansible ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/ansible
